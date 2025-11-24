f5c8127c96b1de20d215bfb769f7f0a5cd29bb45
Ngoc Thach <ngocthachtruongnguyen@gmail.com>
2025-11-25 00:36:49 +0700

    fix: email services

--- src/services/mail.services.js
+++ src/services/mail.services.js
@@ -2,11 +2,16 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS.trim(), // Trim space
     },
+    tls: {
+        rejectUnauthorized: false
+    }
 });
 
 const sendResetEmail = async (email, message) => {
